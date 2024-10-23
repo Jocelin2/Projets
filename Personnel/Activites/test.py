c9926b8fb28ee6b9f510b3642fa2fdbbefa8d95d
Joc <jocelinlauret2@gmail.com>
2024-10-23 18:58:22 +0200

    maj

--- Personnel/Activites/test.py
+++
@@ -1,6 +0,0 @@
-import polyline
-
-def decode_polyline(poly):
-    return polyline.decode(poly)
-
-print(decode_polyline("ikqfIinvyB_@Ho@DmAUcAm@eAsAMg@Si@@k@LgAJsB?cAG~AWfDKIW]O[W_@ILq@d@?HVVNX`@tAZ`@x@Xj@NzAv@|@\\VDx@MvCoAK\\OlBQfA?FDDpAb@HJFB@Hc@jB[v@K^?NFXZf@Pf@t@zCZ`CDjA^xCDJJBNOd@cAd@qA\\uALWGNOx@Ol@Wn@o@vAo@bBaAjBAFJNb@Lv@@fAJVFTRFPXrABp@Et@u@pD?ZDr@XlBA`@GTo@bBg@|@i@n@i@nA{@~@mAjAcAzBi@bAYpAYlBq@bBo@lCm@nAeB|CaAlFAPfB?VDYDs@KOG_@IC@Cl@SvAEPKJWSiAiBY]]Q_Be@IG_@_@_@q@k@w@a@OUCq@HmAIk@S[Ci@u@mAw@E@IVPrC@zD\\hBv@rCBVA`@M@mB{Aw@W]Co@Mc@u@[Sg@i@YQq@YYWYe@Oi@UWcAy@k@a@}ByBo@Yi@YyA_AOSI]Bc@TkBt@yDT}A@YEAe@f@e@Tg@@mAXYTo@x@YHcAM]S_@a@eAcCUiAQeCJoAJYLmAFm@@m@Fc@Py@N_@h@k@r@wAVA^JrAJv@VnBXr@@TAGDqCSQIo@QGEEKBQnAiAf@w@Re@\\a@Uf@qAlBk@^g@d@OB{AHIDHQJD`AAtBwAhC}DZQfC_@f@_@rA_BFCnA@HGFKFa@HyAE[q@sAu@aB}@cB?BN\\`@nARv@R`@T\\RRXDNUZ{BHuCJ[P?r@X`@Hd@`@d@GRKNACBy@NI?QKSQYMsAa@EIEq@AoAMmC?gAPk@lAsA^YJEhA@z@EFCTWp@gA@UEUBLDFRAd@q@tAyBlAsAJWFo@DeAA}@JgAAo@Is@YqAUe@Sq@Ug@OUm@aBIg@C}@?iCAKEGIGW?OEc@OEE_@OGYOqA"))
--- Personnel/Activites/wsgi.py
+++
@@ -1,4 +0,0 @@
-from download_activities import app_flask
-
-if __name__ == '__main__':
-    app_flask.run()
